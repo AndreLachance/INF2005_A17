5417c711ab05a856216c4b7b7ec65ab676349597
Andre Lachance <lachance.andre@ucs.inrs.ca>
2017-10-11 00:42:13 -0400

    Solutions du laboratoire sur le Multimedia

---
+++ Multimedia/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Acceuil</title>
+</head>
+<body>
+	<div>
+		<img width="500" height="333" src="static/img/fork.jpg" alt="Fork in the road"
+			usemap="#acceuil">
+
+		<map name="acceuil">
+			<area shape="rect" coords="0,0,250,333" href="videoJacques.html"
+				alt="redirection vers le video">
+			<area shape="circle" coords="250,176,100" href="audio.html"
+				alt="redirection vers l'audio">
+		</map>
+	</div>
+</body>
+</html>
